ac08069e46338cf14f2f88f2e21ae5c7fafc90e5
Roosevelt Ubaldo <roosevelt.ubaldo@outlook.com>
2021-08-15 22:34:28 -0500

    travis config added

--- platform/node/scripts/publish.sh
+++ platform/node/scripts/publish.sh
@@ -4,16 +4,21 @@ set -e
 set -o pipefail
 
 PACKAGE_JSON_VERSION=`node -e "console.log(require('./package.json').version)"`
+GITHUB_TOKEN=$1
 
 if [[ "${CIRCLE_TAG}" == "node-v${PACKAGE_JSON_VERSION}" ]] || [[ "${PUBLISH:-}" == true ]]; then
     # Changes to the version targets here should happen in tandem with updates to the
     # EXCLUDE_NODE_ABIS property in cmake/node.cmake and the "node" engines property in
     # package.json.
-    for TARGET in 6.0.0 8.0.0 10.0.0; do
+    for TARGET in 10.0.0; do
         if [[ "${BUILDTYPE}" == "RelWithDebInfo" ]]; then
-            ./node_modules/.bin/node-pre-gyp package publish info --target="${TARGET}"
+            ./node_modules/.bin/node-pre-gyp package 
+            NODE_PRE_GYP_GITHUB_TOKEN=${GITHUB_TOKEN} ./node_modules/.bin/node-pre-gyp-github publish
+            ./node_modules/.bin/node-pre-gyp package info --target="${TARGET}"
         elif [[ "${BUILDTYPE}" == "Debug" ]]; then
-            ./node_modules/.bin/node-pre-gyp package publish info --target="${TARGET}" --debug
+            ./node_modules/.bin/node-pre-gyp package 
+            NODE_PRE_GYP_GITHUB_TOKEN=${GITHUB_TOKEN} ./node_modules/.bin/node-pre-gyp-github publish
+            ./node_modules/.bin/node-pre-gyp package info --target="${TARGET}" --debug
         else
             echo "error: must provide either Debug or RelWithDebInfo for BUILDTYPE"
             exit 1
